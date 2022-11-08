886514f7e5afb66bb6f7b6d7a2aa643a066bfb68
rafi-ahmed <smrafi120@gmail.com>
2022-11-08 18:43:52 +0600

    first commit

--- linkedListFromArray.c
+++ linkedListFromArray.c
@@ -1,22 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 struct Node *createLinkedList(int arr[],int size);
+int searceLinkedlist(struct Node *head, int value);
 struct Node{
 	int data;
 	struct Node *next;
 };
 int main(){
-	int a[]={5,10,30};
-	struct Node *head;
-	head=createLinkedList(a,3);
-	while(head!=NULL){
-		printf("%d ->",head->data);
-		head=head->next;
-	}
-	printf("NULL\n");
+	int a[]={15,30,45,60,80};
+	struct Node *head=NULL;
+	head=createLinkedList(a,5);
+	
+	struct Node *current=head;
+	printf("Index:%d",searceLinkedlist(head,45));
 	return 0;
 }
-	struct Node *createLinkedList(int arr[],int size){
+int searceLinkedlist(struct Node *head, int value){
+	int index=1;
+	while(head!=NULL){
+		if(head->data==value){
+			return index;
+		}
+		index++;
+		head=head->next;
+	}
+	return -1;
+}
+
+
+struct Node *createLinkedList(int arr[],int size){
 		struct Node *head=NULL,*temp=NULL,*current=NULL;
 		int i;
 		for(i=0;i<size;i++){
